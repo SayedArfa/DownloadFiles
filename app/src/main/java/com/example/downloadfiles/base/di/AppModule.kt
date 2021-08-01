12acc56420a8287cbf6cc048e7e5b6659aeef0af
sayed <sarafa@taqeem.gov.sa>
2021-08-01 14:14:25 +0200

    fix di

--- app/src/main/java/com/example/downloadfiles/base/di/AppModule.kt
+++ app/src/main/java/com/example/downloadfiles/base/di/AppModule.kt
@@ -4,9 +4,11 @@ import com.ams.downloadfiles.data.source.remote.network.FileAPIService
 import com.example.downloadfiles.base.utils.ServiceGenerator
 import dagger.Module
 import dagger.Provides
+import javax.inject.Singleton
 
 @Module
 class AppModule {
+    @Singleton
     @Provides
     fun provideFileServiceAPIs(): FileAPIService =
         ServiceGenerator().createService(FileAPIService::class.java)
